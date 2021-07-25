cb26979bd580f0f35dedd41170fad785936a3074
chihi829 <87941772+chihi829@users.noreply.github.com>
2021-07-26 01:42:46 +0900

    Add files via upload

--- final.js
+++ final.js
@@ -1,4 +1,4 @@
-https://github.com/chihi829/prof2/blob/master/final.jsconst pagetopBtn = document.querySelector('.page-top');
+const pagetopBtn = document.querySelector('.page-top');
 pagetopBtn.addEventListener('click', () => {
   window.scrollTo({
     top: 0,
